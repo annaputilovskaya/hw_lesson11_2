76fb6f8f2bdda676e43415e7c48d7b50feaf5b95
putilovskaya <putilovskayaa@mail.ru>
2023-12-24 02:17:42 +0300

    add turn_title to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def turn_caps(line):
     return line.upper()
+
+
+def turn_title(line):
+    """
+    Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
+    """
+    return line.title()
